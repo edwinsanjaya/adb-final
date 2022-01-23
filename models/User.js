d1a16ab51ce758ce775ae03d399f67aa8b304219
wilson <wilson@gdn-commerce.com>
2022-01-23 18:05:48 +0700

    update user table

--- models/User.js
+++ models/User.js
@@ -9,7 +9,8 @@ const User = db.define('user', {
   },
   username: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   hash: {
     type: DataTypes.STRING,
