5541611bf3adc1ca3044e19385eca6ab41c5ed3a
wilson <wilson@gdn-commerce.com>
2022-01-26 22:23:55 +0700

    add auth on create user API

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 const genPassword = require('../../utils/passwordUtils').genPassword;
 const passport = require('passport');
 const router = express.Router();
+const isAuth = require('../middleware/authMiddleware').isAuth;
 
 const User = require('../../models/User');
 
-router.post('/', (req, res) => {
+router.post('/', isAuth, (req, res) => {
   const saltHash = genPassword(req.body.pw);
 
   const salt = saltHash.salt;
