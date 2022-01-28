29fea2b3c73e2a88db68cb098c59223513f97a02
Edwinsky <sanjayaedwin@gmail.com>
2022-01-28 11:33:55 +0700

    Login Page & Cleaning

--- client/src/App.js
+++ client/src/App.js
@@ -41,27 +41,4 @@ class App extends Component {
   }
 }
 
-
-// function App() {
-//   return (
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <div className="App">
-//           <AppNavbar />
-//           <Container>
-//             <Routes>
-//               <Route exact path="/" element={<><div>This is home page</div></>} />
-//               <Route exact path="/actor-list" element={<><ActorModal /><ActorList /></>} />
-//               <Route exact path="/article/edit" element={<TinyMceEditor />} />
-//               <Route exact path="/article/:id" element={<ArticleDetailPage/>} />
-//               <Route exact path="/ruangan-rahasia/login" element={<LoginPage/>}/>
-//             </Routes>
-//           </Container>
-//         </div>
-//       </BrowserRouter>
-//     </Provider>
-
-//   );
-// }
-
 export default App;
