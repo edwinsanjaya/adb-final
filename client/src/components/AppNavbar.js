91d5c7806fd8bd7d763fff2f12a30b5b9b0f8720
Edwinsky <sanjayaedwin@gmail.com>
2022-02-27 01:59:58 +0800

    fix username header

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -37,7 +37,7 @@ class AppNavbar extends Component {
     const isAuthenticated = this.props.auth.isAuthenticated
     return (
       <div>
-        {isAuthenticated && <Container>'Logged in as ' + this.props.auth.user.username</Container>}
+        {isAuthenticated && <Container>{'Logged in as ' + this.props.auth.user.username}</Container>}
         <Navbar color="dark" dark expand="sm" container>
           <NavbarBrand href="/">Jellyfish Gaming</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
